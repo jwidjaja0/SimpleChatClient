bacdf41a5a3e9ca76bc6904b5a69e1a874c4a8c3
jwidjaja0@saddleback.edu <jwidjaja@saddleback.edu>
2020-04-28 17:08:21 -0700

    added Landing page

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -4,6 +4,8 @@ import com.SimpleChat.Messages.Packet;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -14,22 +16,34 @@ public class Client implements Runnable{
     private ClientReceive clientReceive;
     private ClientPublish clientPublish;
 
+    private List<Chatroom> chatroomList;
     private BlockingQueue<Packet> outgoingQueue;
     private BlockingQueue<Packet> incomingQueue;
 
     public Client() throws IOException {
         serverConnection = new Socket("localhost", 8000);
 
+        chatroomList = new ArrayList<>();
         outgoingQueue = new ArrayBlockingQueue<>(100);
         incomingQueue = new ArrayBlockingQueue<>(100);
 
         clientReceive = new ClientReceive(serverConnection, incomingQueue);
-        clientPublish = new ClientPublish(incomingQueue);
+        //clientPublish = new ClientPublish(incomingQueue);
 
     }
 
     @Override
     public void run() {
 
+        while(true){
+            try {
+                incomingQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
+
